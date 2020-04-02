4fda67cdfb1406ebdf2ba594debb8f5a3f68bb3c
cace92 <cace92_2@hotmail.it>
2020-04-02 15:09:47 +0200

    Added students and retired transition matrices

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 
-# Setting values----------------------------------------------------------------
-# Environment properties
+import numpy as np
+# SETTING VALUES----------------------------------------------------------------
+# Environment properties--------------------------------------------------------
 Xdim = 10
 Ydim = 10
 
@@ -19,3 +20,69 @@ N_shops = 1
 N_transports = 2
 
 N_places = N_schools + N_jobs + N_sports + N_shops + N_transports
+
+# Transition matrices-----------------------------------------------------------
+# Order: Home, Schools, Jobs, Sports, Shops, Transports
+# The probabilities of remaining in same place for the next step is always 1/2 except for transports
+
+# Students
+# Always use public transports
+# Probabilities: (moving from transports' locations)
+#  Home Schools Jobs Sports Shops
+prob = [0.30, 0.40, 0, 0.20, 0.10]
+stud_trans_mat = []
+N_places_list = [1, N_schools, N_jobs, N_sports, N_shops]
+from_trans_list = [0]*(1+N_places)
+pos = 0
+for i in range(len(N_places_list)):
+    choice = pos + np.random.randint(0, N_places_list[i])
+    from_trans_list[choice] = prob[i]
+    pos += N_places_list[i]
+temp = []
+for i in range(N_places+1):
+    if i < (N_places+1-N_transports):
+        temp.append(0)
+    else:
+        temp.append(0.5/N_transports)
+for i in range(N_places+1-N_transports):
+    temp_ = temp[:]
+    temp_[i] = 0.5
+    stud_trans_mat.append(temp_)
+for i in range(N_transports):
+    stud_trans_mat.append(from_trans_list)
+
+# Workers
+# Never use public transports
+# Probabilities: (moving from places locations)
+#  Home Schools Jobs Sports Shops
+
+
+# Retireds
+# Always use public transports
+# Probabilities: (moving from transports' locations)
+# Home Schools Jobs Sports Shops
+prob = [0.5, 0, 0, 0.10, 0.40]
+ret_trans_mat = []
+N_places_list = [1, N_schools, N_jobs, N_sports, N_shops]
+from_trans_list = [0]*(1+N_places)
+pos = 0
+for i in range(len(N_places_list)):
+    choice = pos + np.random.randint(0, N_places_list[i])
+    from_trans_list[choice] = prob[i]
+    pos += N_places_list[i]
+temp = []
+for i in range(N_places+1):
+    if i < (N_places+1-N_transports):
+        temp.append(0)
+    else:
+        temp.append(0.5/N_transports)
+for i in range(N_places+1-N_transports):
+    temp_ = temp[:]
+    temp_[i] = 0.5
+    ret_trans_mat.append(temp_)
+for i in range(N_transports):
+    ret_trans_mat.append(from_trans_list)
+ret_trans_mat[0][0] = 0.7
+for i in range(N_transports):
+    ret_trans_mat[0][N_places-N_transports+1+i] = 0.3/N_transports
+print(ret_trans_mat)
